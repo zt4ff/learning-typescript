9c7b740b1263ca3eb10aec38c7a692adb3c96250
Kayode Oluwasegun <segunkayode00@gmail.com>
2022-02-06 23:36:15 +0100

    cleanup: email sender function

---
+++ quotes-api/src/helpers/messages.ts
@@ -0,0 +1,6 @@
+export const messages = {
+  // emails messsages
+  SMTP_CREDENTIAL_ERROR: 'please provide SMTP server credentials',
+  VALID_EMAIL_ERROR: 'expects a valid email',
+  EMAIL_SENT_SUCCESS: 'email sent successfully',
+};
